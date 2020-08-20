87b30fd350497143e127c4204ff7a0f301b94e8b
Shivam Rana <shivam4learning@gmail.com>
2020-08-20 12:47:06 +0530

    Completed 14 august 2020 lab work

--- 14_aug_oop_lab/student.cpp
+++ 14_aug_oop_lab/student.cpp
@@ -1,6 +1,45 @@
 #include<iostream>
 using namespace std;
+class Student
+{
+    int admno;
+    char sname[20];
+    class Student_Marks
+    {
+        float eng, math, sci, csci, hi, total;
+        public: 
+        void sMarks()
+        {
+            cout<<"Enter the marks of english, maths , science, computer science, hindi respectively: "<<endl;
+            cin>>eng>>math>>sci>>csci>>hi;
+        }
+        float cTotal()
+        {
+            total = eng+ math+ sci + csci + hi;
+            return total;
+        }
+    };
+    public:
+    Student :: Student_Marks s;
+    void readData()
+    {
+        cout<<"Enter the student name: ";
+        cin.get(sname, 20);
+
+        cout<<"Enter the admission number: ";
+        cin>>admno;
+        s.sMarks();
+    }
+    void showData()
+    {
+        cout<<"Total marks of student: "<<s.cTotal();
+    }
+
+};
 int main()
 {
+    Student s;
+    s.readData();
+    s.showData();
     return 0;
 }
