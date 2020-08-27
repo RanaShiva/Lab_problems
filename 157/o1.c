342323407bb84fbdbd09e60f21a7f865c8e29bf0
Shivam Rana <shivam4learning@gmail.com>
2020-08-27 21:39:29 +0530

    Added some code for data srtucture lab 157

--- 157/o1.c
+++ 157/o1.c
@@ -1,8 +1,18 @@
 /*
-Q8.Evaluate the following expression Assume, int i = 10, b=20,c=5,e=2; float q =2.5, d=4.5;
+Q8.Evaluate the following expression Assume, int a = 10, b=20,c=5,e=2; float q =2.5, d=4.5;
  i) a/b + (a/(2*b))
  ii) f = ++a + b-- / q find value of f, a, and b 
  ii) G = b%a++; find value of G and a
   iv) H = b%++a; find value of H and a 
   v)  a+2 < b || !c && a==d || a-2 <=e
 */
+
+#include<stdio.h>
+int main()
+{
+  int a=10, b=20, c=5, e=2;
+  float q=2.5, d=4.5;
+  printf("%d\n",a/b+(a/(2*b)));
+  printf("%f\n",++a+b--/q);
+  return 0;
+}
